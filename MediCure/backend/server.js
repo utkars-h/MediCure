4fdbcb027aee91220486f81884d1e28a3a3c907a
utkars-h <utk.akt@gmail.com>
2022-03-15 02:53:44 +0530

    Updated

--- MediCure/backend/server.js
+++ MediCure/backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import connectDB from './config/db.js'    //.js for files in newer ECMA
 
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const app = express()
 
@@ -16,6 +17,10 @@ app.get('/', (req, res)=> {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT , console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.magenta.bold))
