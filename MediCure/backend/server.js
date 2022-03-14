bcc87b7fb5045d5220f4990e4fc0ae252ddfc8a2
utkars-h <utk.akt@gmail.com>
2022-03-14 23:08:04 +0530

    Working Database

--- MediCure/backend/server.js
+++ MediCure/backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import connectDB from './config/db.js'
-import products from './data/products.js'  //.js for files in newer ECMA
+import connectDB from './config/db.js'    //.js for files in newer ECMA
+
+import productRoutes from './routes/productRoutes.js'
+
 const app = express()
 
 dotenv.config()
@@ -13,13 +15,7 @@ app.get('/', (req, res)=> {
     res.send("API is running...")
 })
 
-app.get('/api/products', (req,res)=>{
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req,res)=>{
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
 const PORT = process.env.PORT || 5000
 app.listen(PORT , console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.magenta.bold))
