3a8e44744bdffeb736a6eda9b07d9041cb60d1a5
Sangaja Patel <patelsangaja06@gmail.com>
2022-03-23 13:55:00 +0530

    getOrderByID, order reducer and action

--- MediCure/frontend/src/store.js
+++ MediCure/frontend/src/store.js
@@ -6,7 +6,10 @@ import { productListReducer, productDetailsReducer} from './reducers/productRedu
 import { cartReducer } from './reducers/cartReducers.js'
 import { userLoginReducer, userRegisterReducer,userDetailsReducer,
     userUpdateProfileReducer, } from './reducers/userReducers.js'
-import { orderCreateReducer } from './reducers/orderReducers'    
+    import {
+      orderCreateReducer,
+      orderDetailsReducer,
+    } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -17,6 +20,7 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
