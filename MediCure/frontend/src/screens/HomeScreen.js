30715965bbc838e610360b19735fcd64f22c51d0
utkars-h <utk.akt@gmail.com>
2022-03-26 23:35:12 +0530

    Updated with minor frontend improvements

--- MediCure/frontend/src/screens/HomeScreen.js
+++ MediCure/frontend/src/screens/HomeScreen.js
@@ -43,7 +43,7 @@ const HomeScreen = ({ match }) => {
         <>
         <Row>
           {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Col key={product._id} sm={12} md={5} lg={3} xl={4}>
               <Product product={product} />
             </Col>
           ))}
