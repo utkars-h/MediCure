ed11ec29d3606b4ef6adf17df8684c0ad4cb4d8e
utkars-h <utk.akt@gmail.com>
2022-03-28 14:20:36 +0530

    Minor Typo

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
